8fdba7b98d8360dc7e41dba01ddf6965a6dfb1ee
Titus Nicolae <nicolaetitus12@gmail.com>
2012-06-11 20:52:47 +0200

    something else

--- bench.py
+++ bench.py
@@ -2,7 +2,9 @@ import os
 from glob import glob
 import shlex
 import hashlib 
-
+from cStringIO import StringIO
+import sys
+from multiprocessing import Process,Queue
      
 def base64_md5_file(f,block_size=2**10):
     md5=hashlib.md5()
@@ -23,15 +25,38 @@ def read_hash(hashfile="hash"):
         return h
     except:
         return {}
+
+def process(q,pyfile):
+    sys.stdout=temp=StringIO()
+    
+    execfile(pyfile,{}) 
+    q.put((    
+    
+    
      
 if __name__=="__main__":
     h=read_hash()
+    q=Queue()
+    old_stdout=sys.stdout
+    output={}
+    processes=[]
     for sagefile in sorted(glob("sage/*.sage")):
         pyfile=sagefile[:-4]+"py"
-        print sagefile
-        f=open(sagefile)
+        f=open(sagefile,"r")
         hashvalue=base64_md5_file(f)
         if (sagefile in h and hashvalue != h[sagefile] ) or sagefile not in h:
             h[sagefile]=hashvalue
-            os.system("sage -preparse "+sagefile)
-        execfile(pyfile,{}) 
+         #   os.system("sage -preparse "+sagefile)
+        var={}
+        print sagefile
+        sys.stdout=temp=StringIO()
+        p=Process(target=process,args=(pyfile,q))
+        p.start()
+         
+        execfile(pyfile,{})
+        output[sagefile]=temp.getvalue()
+        sys.stdout=old_stdout 
+        print temp.getvalue()[:-1] #don't print newline again
+        temp.close()
+    for p in processes:
+        p.join()
--- sage/r01_1.sage
+++ sage/r01_1.sage
@@ -1,3 +1,3 @@
 def f(z): 
     return sqrt(1/3)*z^2 + i/3
-time real(f(f(f(f(f(f(f(f(i/2)))))))))
+time real(f(f(f(f(i/2)))))
